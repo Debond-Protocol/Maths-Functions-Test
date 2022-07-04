f3afaba3c2b61b94b06ac290c51d422a8c47061e
drvarun <12458730+drvarun@users.noreply.github.com>
2022-07-04 13:58:35 +0200

    updated tests

--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -10,6 +10,12 @@ def math_function_contract():
 
 
 def test_read_contract(math_function_contract):
-    # pp = MathFunctions[-1]
-    output = math_function_contract.getLockedBalance(100, 100, 100)
-    assert output == 95
+    o1 = math_function_contract.getLockedBalance(100, 100, 100)
+    o2 = math_function_contract.getLockedBalance(100, 4, 4)
+    o3 = math_function_contract.getLockedBalance(100, 5, 5)
+    o4 = math_function_contract.getLockedBalance(100, 6, 6)
+    o5 = math_function_contract.getLockedBalance(100, 50, 100)
+    o6 = math_function_contract.getLockedBalance(1000, 50, 50)
+    o7 = math_function_contract.getLockedBalance(1000, 51, 51)
+
+    assert [o1, o2, o3, o4, o5, o6, o7] == [95, 0, 0, 1, 90, 0, 1]
